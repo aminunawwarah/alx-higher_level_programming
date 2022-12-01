23723e14556567449fa69ea8c9cf866a21877b27
aminunawwarah <aminunawwarah@gmail.com>
2022-11-30 17:42:14 -0800

    Last Upadate Python Hello

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"
-word3 = f"{word[0:3]}"
-word2 = f"{word[-2:]}"
-middle_word = f"{word[1:-1]}"
-print(f"First 3 letters: {word3}")
-print(f"Last 2 letters: {word2}")
-print(f"Middle word: {middle_word}")
+word = 'Holberton'
+word3 = f'{word[0:3]}'
+word2 = f'{word[-2:]}'
+middle_word = f'{word[1:-1]}'
+print(f'First 3 letters: {word3}')
+print(f'Last 2 letters: {word2}')
+print(f'Middle word: {middle_word}')
