8aac2d999d06c949e712145ed6a1d8890b196b0a
aminunawwarah <aminunawwarah@gmail.com>
2023-01-02 01:53:21 -0800

    Python Test-Driven Program

---
+++ 0x07-python-test_driven_development/tests/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module containing a dummy adder function for testing"""
+
+
+def add_integer(a, b=98):
+    """ sumup integers
+        Parameters:
+        @a: first integer
+        @b: second integer, by default 98
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
